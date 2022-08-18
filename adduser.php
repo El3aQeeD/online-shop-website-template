cce5612508560afedfdad9f9d30aa8d9fc7e677c
Khaled Mostafa <95879454+El3aQeeD@users.noreply.github.com>
2022-08-19 01:01:44 +0200

    Add files via upload

---
+++ adduser.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once 'db.php';
+$name=$_REQUEST["name"];
+$phone=$_REQUEST["phone"];
+$address=$_REQUEST["address"];
+$email=$_REQUEST["email"];
+$password=$_REQUEST["password"];
+
+$sql="INSERT INTO user (FullName,Address,PhoneNumber,UserTypeId,Email,Password) VALUES ('$name','$address','$phone',1,'$email','$password')";
+$conn->query($sql);
+echo $conn->insert_id;
+?>
