faaf40d4a833d67809e10dad8ea6ef617ed1ef51
Khaled Mostafa <95879454+El3aQeeD@users.noreply.github.com>
2022-08-22 00:54:15 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+include_once 'db.php';
+include_once 'header.php';
+$count=0;
+?>
+<!-- Products Start -->
+<div class="container-fluid pt-5 pb-3">
+<h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span class="bg-secondary pr-3">Featured Products</span></h2>
+<div class="row px-xl-5">
+<?php 
+$sql="SELECT * FROM product";
+$execute=$conn->query($sql);
+
+while($Data=$execute->fetch_assoc()){
+
+?>
+    
+        
+        
+            <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                <div class="product-item bg-light mb-4">
+                    <div class="product-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src=<?php echo $Data["Photo"];?> alt="">
+                        <div class="product-action">
+                            <a class="btn btn-outline-dark btn-square" href="productdetail.php?PID=<?php echo $Data["Id"];?>"><i class="fa fa-shopping-cart"></i></a>
+                            
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <a class="h6 text-decoration-none text-truncate" href=""><?php echo $Data["Name"]; ?></a>
+                        <div class="d-flex align-items-center justify-content-center mt-2">
+                            <h5><?php echo $Data["Price"]; ?> L.E</h5>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        
+    
+
+<!-- Products End -->
+<?php
+/*
+$count++;
+if($count>4){
+    $count=0;
+    echo "</div>" ;
+    
+
+}
+*/
+}
+?>
+        </div>
+</div>
+
+<?php
+include_once 'footer.php';
+?>
---
+++ usercart.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include_once 'db.php';
+$PID=$_REQUEST["PID"];
+
+
+
+if(isset($_SESSION["UserId"])){
+$UserId=$_SESSION["UserId"];
+$sql2="INSERT into user_product (BId,PId) Values ($UserId,$PID)";
+$execute2=$conn->query($sql2);
+
+}
+
+
+if(isset($_SESSION["UserId"])){
+    $sql="SELECT * FROM user_product WHERE BId=".$_SESSION["UserId"];
+    $row=$conn->query($sql);
+    echo $row->num_rows;
+    }
+    else
+    {
+        echo "0";
+    }
+
+
+?>
